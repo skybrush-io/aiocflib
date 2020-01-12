a56650c628502f8bf1c7655b5c4b3316b41e1a73
Tamas Nepusz <ntamas@gmail.com>
2020-01-12 11:52:11 +0100

    exporting _version__ and __version_info__

--- aiocflib/__init__.py
+++ aiocflib/__init__.py
@@ -1,5 +1,6 @@
 """asyncio-based variant of the Crazyflie Python library."""
 
 from .errors import error_to_string
+from .version import __version__, __version_info__
 
-__all__ = ("error_to_string",)
+__all__ = ("error_to_string", "__version__", "__version_info__")
