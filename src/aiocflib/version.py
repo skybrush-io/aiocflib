8f66eb1d902d5d72806d130d9f887549268fd794
Tamas Nepusz <ntamas@gmail.com>
2020-09-28 23:49:36 +0200

    bumped version to 0.8.3

--- src/aiocflib/version.py
+++ src/aiocflib/version.py
@@ -1,4 +1,4 @@
 """Version information for the asycio-based Crazyflie Python library."""
 
-__version_info__ = (0, 8, 2)
+__version_info__ = (0, 8, 3)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
