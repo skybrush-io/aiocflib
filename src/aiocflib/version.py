35ed9d350d1c9de0f070394cd0a48f43b31af588
Tamas Nepusz <ntamas@gmail.com>
2021-09-01 14:18:56 +0200

    chore: bumped version to 2.3.0

--- src/aiocflib/version.py
+++ src/aiocflib/version.py
@@ -1,4 +1,4 @@
 """Version information for the asycio-based Crazyflie Python library."""
 
-__version_info__ = (2, 2, 1)
+__version_info__ = (2, 3, 0)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
